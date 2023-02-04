e22c6b589fca2c67d56fb924c1497ffab1de7399
Zitouni Mohamed <m3d.zitouni@gmail.com>
2023-02-04 16:36:26 +0100

    Migration and Models

---
+++ database/migrations/2023_01_29_143444_create_customer_addresses_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('customer_addresses', function (Blueprint $table) {
+            $table->id();
+            $table->string('address1', 45);
+            $table->string('address2', 45);
+            $table->string('city', 255);
+            $table->string('state', 45)->nullable();
+            $table->string('zipcode', 45);
+            $table->string('country_code', 3);
+            $table->foreignId('customer_id')->references('id')->on('customers');
+            $table->timestamps();
+            $table->foreign('country_code')->references('code')->on('countries');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('customer_addresses');
+    }
+};
